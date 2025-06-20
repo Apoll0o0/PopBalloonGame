08c00f00e7a5277c81b8a1737fae5c0674348792
Alperen Beyhan <narutozaxd43@gmail.com>
2025-06-20 17:52:25 +0300

    Balon Spawnı Scripti yapıldı

--- Assets/Scripts/BalloonGenerator.cs
+++ Assets/Scripts/BalloonGenerator.cs
@@ -1,18 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BalloonGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+
+    TextMeshProUGUI killedText;
+
+    [SerializeField]
+
+    TextMeshProUGUI leackedText;
+
+    [SerializeField]
+
+    TextMeshProUGUI gameOverText;
+
+    [SerializeField]
+
+    GameObject balloonPrefab;
+
+    [HideInInspector]
+    public int killedCount = 0;
+
+    [HideInInspector]
+    public int leackedCount = 0;
+
+    float spawnDelay = 0.5f;
+
+    float velocity = 80f;
+
+    bool gameIsOver = false;
+
+    public bool isOnMenu = false;
+
+    int maxHP = 10;
+
+    int curHP;
+
+    Rigidbody2D rb;
+
+    AudioSource audioSource;
+
+    private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        curHP = maxHP;
+        StartCoroutine(Spawn());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Spawn()
     {
-        
+        SpawnBalloon();
+        yield return new WaitForSeconds(spawnDelay);
+        StartCoroutine(Spawn());
+        yield return null;
     }
+
+    void SpawnBalloon()
+    {
+        Vector2 spawnPosition = new Vector2(Screen.width * Random.Range(0.1f, 0.9f), -200f);
+        GameObject obj = Instantiate(balloonPrefab, spawnPosition, transform.rotation, transform);
+        rb = obj.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0f, velocity);
+        velocity *= 1.001f;
+        spawnDelay *= 0.999f;
+    }
+
+    void GameExit()
+    {
+        Application.Quit();
+    }
+
+    void RestartGame()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowKilledText()
+    {
+        killedText.text = "Patlatýldý: " + killedCount;
+    }
+
+    [SerializeField]
+
+    GameObject menuObj;
+    public void ShowLeackedText()
+    {
+        leackedText.text = "Kaçýrýlan: " + leackedCount;
+        audioSource.Play();
+        curHP--;
+        if(curHP == 0)
+        {
+            gameIsOver = true;
+            menuObj.SetActive(true);
+            Time.timeScale = 0.0f;
+            isOnMenu = true;
+        }
+    }
+
+    public void StartMenu()
+    {
+        if (gameIsOver) return;
+        if (!isOnMenu)
+        {
+            menuObj.SetActive(true);
+            isOnMenu = true;
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            menuObj.SetActive(false);
+            Time.timeScale = 1.0f;
+            isOnMenu= false;
+        }
+    }
+
 }
